de2c8d537063915961104a78110b8de9620fedd5
spencerhhubert <spencerhhubert@gmail.com>
2022-06-08 22:26:25 -0400

    production paypal

--- mat.js
+++ mat.js
@@ -1,4 +1,4 @@
-const normal_paypal_html = `
+const normal_paypal_html_sandbox = `
 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="3RVTR58FG855Q">
@@ -6,8 +6,17 @@ const normal_paypal_html = `
 <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 `
+
+const normal_paypal_html = `
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="7VDKFWJEMFXKG">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+`
 //there's a thing in the button code that lets you add a textbox type thing. we're going to do this for the coupon code tracking
-const discount_paypal_html = `
+const discount_paypal_html_sandbox = `
 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="7K2HGHPJESK52">
@@ -17,8 +26,20 @@ const discount_paypal_html = `
 <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
-
 `
+
+const discount_paypal_html = `
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="F9MF5U8M2S77J">
+<table>
+<tr><td><input type="hidden" name="on0" value="Discount Reason"></td></tr><tr><td><input type="hidden" name="os0" maxlength="200"></td></tr>
+</table>
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+`
+
 const pickup_paypal_html = discount_paypal_html
 
 const discount_and_pickup_html = `
